efe0f68c57b5d058e07253654406be797e4029ac
raiqi1 <116938210+raiqi1@users.noreply.github.com>
2023-02-26 18:13:26 +0700

    Update routes.js

--- tugasEduwork/app/invoice/routes.js
+++ tugasEduwork/app/invoice/routes.js
@@ -3,5 +3,6 @@ const { police_check } = require('../../middlewares');
 const invoiceController = require('./controller');
 
 router.get('/invoice/:order_id',police_check("read", "Invoice"),invoiceController.show);
+// router.get('/invoice/:order_id',police_check("view", "Invoice"),invoiceController.show);
 
 module.exports = router
